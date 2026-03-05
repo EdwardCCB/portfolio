fc94fd519f149efcb48949eca3b452cfa02438bb
Eduardo Marino <eduardomarino1028@hotmail.com>
2026-03-04 21:04:51 -0500

    chore: setup github pages deployment for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://EdwardCCB.github.io',
+  base: '/portfolio',
   integrations: [react()]
 });
